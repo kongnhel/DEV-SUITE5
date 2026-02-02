8dc88208ef4f8299ba504103741c9f538eb76cbc
KongNhel <kabkong94@gmail.com>
2026-02-02 21:42:02 +0700

    first commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const authMiddleware = require('../middleware/auth'); // Middleware ដែលយើងធ្លាប់ធ្វើ
+const multer = require("multer");
+const userController = require("../controllers/userController");
+const authMiddleware = require("../middleware/auth"); // Middleware ដែលយើងធ្លាប់ធ្វើ
 
-router.get('/profile', authMiddleware, (req, res) => {
-    res.render('profile', { 
-        title: 'Neural Profile', 
-        pageKey: 'profile',
-        user: res.locals.user,
-        theme: '#a855f7'
-    });
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.get("/profile", authMiddleware, (req, res) => {
+  res.render("profile", {
+    title: "Neural Profile",
+    pageKey: "profile",
+    user: res.locals.user,
+    theme: "#a855f7",
+  });
 });
 // ប្រើ Middleware ការពារដើម្បីឱ្យប្រាកដថា ទាល់តែ Login ទើបចូលដល់ Controller
-router.post('/update-profile', authMiddleware, userController.updateProfile);
+// router.post('/update-profile', authMiddleware, userController.updateProfile);
+router.post(
+  "/update-profile",
+  upload.single("photo"),
+  userController.updateProfile,
+);
 
-router.post('/delete-account', authMiddleware, userController.deleteAccount)
-
+router.post("/delete-account", authMiddleware, userController.deleteAccount);
 
 module.exports = router;
